49ee4e58c04c26f2de5fa92c4e04d0656c948c43
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-08-18 00:13:17 +0330

    nestedObg & nestedArray created

--- src/components/FormikForm.jsx
+++ src/components/FormikForm.jsx
@@ -1,12 +1,18 @@
 import * as Yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import TextError from "./TextError";
 
 const initialValues = {
   name: "vishwas",
   email: "",
   channel: "",
-  Comments: "",
-  Address: "",
+  comments: "",
+  address: "",
+  social: {
+    facebook: "",
+    twitter: "",
+  },
+  phoneNumber: ["", ""],
 };
 
 const onSubmit = (values) => {
@@ -28,13 +34,11 @@ const FormikForm = () => {
     >
       <section>
         <Form className="container">
-            <h2>Form</h2>
+          <h2>Form</h2>
           <div className="form-control">
             <label htmlFor="channel">Name</label>
             <Field type="text" id="naem" name="name" />
-            <ErrorMessage name="name">
-              {(errorMsg) => <div className="error">{errorMsg}</div>}
-            </ErrorMessage>
+            <ErrorMessage name="name" component={TextError} />
           </div>
           <div className="form-control">
             <label htmlFor="channel">E-mail</label>
@@ -64,15 +68,33 @@ const FormikForm = () => {
             {/* field Revisited */}
             <Field id="address" name="address">
               {(props) => {
-                const {field,form,meta} = props;
+                const { field, form, meta } = props;
                 return (
                   <div>
-                    <input type="text" id="address" {...field}/>
-                    {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                    <input type="text" id="address" {...field} />
+                    {meta.touched && meta.error ? (
+                      <div>{meta.error}</div>
+                    ) : null}
                   </div>
-                )
+                );
               }}
             </Field>
+          </div>
+          <div className="form-control">
+            <label>FaceBook profile</label>
+            <Field type="text" id="facebook" name="social.facebook" />
+          </div>
+          <div className="form-control">
+            <label>Twitter profile</label>
+            <Field type="text" id="twitter" name="social.twitter" />
+          </div>
+          <div className="form-control">
+            <label>Primary Phone number</label>
+            <Field type="text" id="primaryPh" name="phoneNumber[0]" />
+          </div>
+          <div className="form-control">
+            <label>Secondary Phone number</label>
+            <Field type="text" id="secondaryPh" name="phoneNumber[1]" />
           </div>
 
           <button type="submit">Submit</button>
