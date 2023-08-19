e60bf2e7307dd796b374dd545e6fc2f38e98b9fa
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-08-19 17:09:29 +0330

    radoi componet created

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -4,22 +4,30 @@ import FormikControl from "./FormikControl";
 
 function FormikContainer() {
 
-    const dropdownOptions = [
+  const dropdownOptions = [
         {key: 'Select an option' , value: ''},
         {key: 'Option 1' , value: 'option 1'},
         {key: 'Option 2' , value: 'option 2'},
         {key: 'Option 3' , value: 'option 3'},
-    ]
+  ];
+
+  const radioOptions =[
+    {key:'option 1', value: 'rOption1'},
+    {key:'option 2', value: 'rOption2'},
+    {key:'option 3', value: 'rOption3'},
+  ]
 
   const initialValues = {
     email: '',
     description: '',
     selectOption: '',
+    radioOption: '',
   };
   const validationSchema = Yup.object({
     email: Yup.string().email("Invalid email format").required("Required"),
     description: Yup.string().required("Required"),
     selectOption: Yup.string().required("Required"),
+    radioOption: Yup.string().required("Required"),
   });
   const onSubmit = (values) => console.log("Form_Data :", values);
   return (
@@ -30,6 +38,7 @@ function FormikContainer() {
             <FormikControl control='input' type='email' label='E-mail' name='email' />
             <FormikControl control='textarea' label='Description' name='description' />
             <FormikControl control='select' label='Select a topic' name='selectOption' options={dropdownOptions} />
+            <FormikControl control='radio' label='Radio Topic' name='radioOption' options={radioOptions} /> 
             <button type="submit">Submit</button>
         </Form>
         }
--- src/components/FormikControl.js
+++ src/components/FormikControl.js
@@ -1,4 +1,5 @@
 import Input from "./Input";
+import RadioButtons from "./RadioButtons";
 import Select from "./Select";
 import Textarea from "./Textarea";
 
@@ -11,7 +12,8 @@ function FormikControl(props) {
       return <Textarea {...rest} />;
     case "select":
       return <Select {...rest} />;
-    case "redio":
+    case "radio":
+      return <RadioButtons {...rest} />
     case "checkbox":
     case "date":
     default:
