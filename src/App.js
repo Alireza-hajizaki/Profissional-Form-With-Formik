f56d68daa7e87f469447079974b769eb0ea0ae62
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-08-20 18:05:27 +0330

    enrollment form created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import FormikContainer from './components/FormikContainer';
 import RegistrationForm from './components/RegistrationForm';
 import LoginForm from './components/LoginForm';
+import EnrollmentForm from './components/Enrollment';
 
 function App() {
   return (
     <div className="app">
       {/* <FormikContainer/> */}
       {/* <LoginForm/> */}
-      <RegistrationForm/>
+      {/* <RegistrationForm/> */}
+      <EnrollmentForm/>
     </div>
   );
 }
